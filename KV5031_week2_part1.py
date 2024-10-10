89bb1d6424eb12ae4d0c5426229308d3d2e705ce
Endeavour <martin.wonders@unn.ac.uk>
2024-10-10 19:49:28 +0100

    Good Animals + tortoise

--- KV5031_week2_part1.py
+++ KV5031_week2_part1.py
@@ -25,9 +25,20 @@ class Cat(Animal):
     def display_info(self):
         print(f"Name: {self.name}, Colour: {self.colour}")
 
+class Tortoise(Animal):
+    def __init__(self, name: str, colour: str):
+        super().__init__(name, colour)
+
+    def speak(self):
+        return "Sigh!"
+
+    def display_info(self):
+        print(f"Name: {self.name}, Colour: {self.colour}")
+
 # Example usage:
 dog = Dog("Buddy", "Golden", "Golden Retriever")
 cat = Cat("Whiskers", "Black")
+tortoise = Tortoise("Flash", "Brown")
 
 # Display information and speak
 dog.display_info() # Output: Name: Buddy, Species: Dog, Breed: Golden Retriever
@@ -35,3 +46,6 @@ print(dog.speak()) # Output: Woof!
 print("---")
 cat.display_info() # Output: Name: Whiskers, Species: Cat, Color: Black
 print(cat.speak()) # Output: Meow!
+print("---")
+tortoise.display_info()
+print(tortoise.speak())
