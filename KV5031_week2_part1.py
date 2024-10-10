01ccf7667ec63b07d3dd545da945be411f806ddb
Endeavour <martin.wonders@unn.ac.uk>
2024-10-10 17:25:45 +0100

    Base Car class with a Truck child class

--- KV5031_week2_part1.py
+++ KV5031_week2_part1.py
@@ -1,59 +1,30 @@
-class Address:
-    def __init__(self, street, city, postal_code):
-        self.street = street
-        self.city = city
-        self.postal_code = postal_code
+class Car():
+    def __init__(self, make, model, year, number_of_wheels, number_of_doors):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.number_of_wheels = number_of_wheels
+        self.number_of_doors = number_of_doors
 
-    def return_address(self):
-        return f"{self.street}, {self.city}, {self.postal_code}"
+    def display_info(self):
+        print(f"Make: {self.make}")
+        print(f"Model: {self.model}")
+        print(f"Year: {self.year}")
+        print(f"Number of wheels: {self.number_of_wheels}")
+        print(f"Number of Doors: {self.number_of_doors}")
 
+class Truck(Car):
+    def __init__(self, make, model, year, number_of_wheels ,number_of_doors, payload_capacity):
+        super().__init__(make, model, year, number_of_wheels, number_of_doors)
+        self.payload_capacity = payload_capacity
 
-class Author:
-    def __init__(self, name, email, phone):
-        self.name = name
-        self.email = email
-        self.phone = phone
+    def display_info(self):
+        super().display_info()
+        print(f"Payload Capacity: {self.payload_capacity} tons")
 
-    def return_info(self):
-        return f"{self.name}, {self.email}, {self.phone}"
+car:Car = Car("Toyota", "Corolla", 2022, 4, 4)
+a_truck:Truck = Truck("Ford", "F-150", 2023, 4, 3, 3.3 )
+car.display_info()
+print("----------------")
+a_truck .display_info()
 
-class Publisher:
-    def __init__(self, name, address, email, phone):
-        self.name = name
-        self.address = address
-        self.email = email
-        self.phone = phone
-
-    def return_info(self):
-        return f"{self.name}, {self.address.return_address()}, {self.email}, {self.phone}"
-
-
-# Good aggregation example for Book class
-class Book:
-    def __init__(self, title, author, publisher, isbn, num_pages):
-
-        self.title = title
-        self.author = author
-        self.publisher = publisher
-        self.isbn = isbn
-        self.num_pages = num_pages
-
-    def show_info(self):
-        print(f"Title: {self.title}")
-        print(f"Author: {self.author.return_info()}")
-        print(f"Publisher: {self.publisher.return_info()}")
-        print(f"ISBN: {self.isbn}")
-        print(f"Number of Pages: {self.num_pages}")
-
-#Create and Address object for the publisher
-publisher_address = Address("123 Publisher Ave", "New York", "NY")
-#Create a publisher object and use the address
-publisher = Publisher("Scribner", publisher_address, "contact@scribner.com", "987-654-3210")
-#Create an Author object
-author = Author("F. Scott Fitzgerald", "fscott@example.com", "123-456-7890")
-
-# Creating a Book object with the author and publisher objects
-book = Book( "The Great Gatsby", author, publisher, "9780743273565", 180)
-
-# Display book information
-book.show_info()
